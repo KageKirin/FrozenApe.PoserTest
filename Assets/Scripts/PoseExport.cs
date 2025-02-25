79f6faa746fd67b7b10f1e59e6708566a8386125
KageKirin <kagekirin@gmail.com>
2025-02-25 15:38:56 +0900

    feat (PoseExport): implement Pose state functionality

--- Assets/Scripts/PoseExport.cs
+++ Assets/Scripts/PoseExport.cs
@@ -20,5 +20,54 @@ namespace Game
         }
 
         public State state = State.Idle;
+        public GameObject targetGameObject;
+        public PosedBoneContainer posedBones;
+
+        void Start()
+        {
+            targetGameObject = GameObject.Find("Ethan");
+            Debug.AssertFormat(
+                targetGameObject != null,
+                "could not retrieve GameObject {0}",
+                targetGameObject
+            );
+
+            var path = $"{Application.dataPath}/apose.json";
+            var json = File.ReadAllText(path);
+            if (string.IsNullOrEmpty(json))
+            {
+                Debug.LogError($"Failed to read {path}.");
+                return;
+            }
+
+            posedBones = JsonSerialization.FromJson<PosedBoneContainer>(json);
+            if (posedBones == null || posedBones.bones.Count == 0)
+            {
+                Debug.LogError($"Could not read posed bones in {path}.");
+                return;
+            }
+
+            if (targetGameObject != null)
+            {
+                state = State.Pose;
+            }
+        }
+
+        void Update()
+        {
+            if (state == State.Idle)
+                return;
+
+            if (state == State.Pose)
+            {
+                IRigPuppeteer rigPuppeteer = new RigPuppeteer();
+                var transforms = targetGameObject.GetComponentsInChildren<Transform>(
+                    includeInactive: true
+                );
+
+                rigPuppeteer.Pose(transforms, posedBones.bones);
+
+                state = State.Idle;
+            }
     }
 }
